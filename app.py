2d6b78b11d42a68ed415b291a6da6a860cde3035
SumathiMathiyazhakan <158417784+SumathiMathiyazhakan@users.noreply.github.com>
2024-02-01 15:11:07 +1100

    Update app.py

--- app.py
+++ app.py
@@ -3,39 +3,7 @@ import pandas as pd
 import joblib
 from utils import preprocessor
 
-custom_css = """
-    <style>
-        body {
-            background-color: #f0f0f0; /* Set your desired background color */
-            font-family: 'Arial', sans-serif; /* Set your desired font */
-        }
-        .stApp {
-            color: #333; /* Set your desired text color */
-        }
-        .stTextInput, .stSelectbox, .stTextArea {
-            background-color: #ffffff; /* Set the background color for input elements */
-            color: #333; /* Set the text color for input elements */
-        }
-        .stButton {
-            background-color: #4CAF50; /* Set your desired button color */
-            color: #ffffff; /* Set the text color for buttons */
-        }
-        /* Add more CSS styles as needed */
-    </style>
-"""
-
-st.markdown(custom_css, unsafe_allow_html=True)
-
-# Your Streamlit app content goes here
-st.title("Customized Streamlit App")
-st.text("This is a sample Streamlit app with a custom color pattern.")
-
 def run():
-
-
-
-
-
     model = joblib.load(open('model.joblib','rb'))
 
     st.title("Sentiment Analysis")
